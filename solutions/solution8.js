042e4a328108cdfa4271c34c5ede6e890a7d7fb5
Marcellino <68014273+marksterben@users.noreply.github.com>
2022-02-21 16:26:19 +0700

    imrpove solution8

--- solutions/solution8.js
+++ solutions/solution8.js
@@ -1,43 +1,46 @@
-const checkPattern = (letter) => /^[a-z0-9]+$/i.test(letter);
+// const checkPattern = (letter) => /^[a-z0-9]+$/i.test(letter);
+
+const checkPattern = (value) => {
+  if ((value >= "a" && value <= "z") || (value >= "A" && value <= "Z")) {
+    return true;
+  }
+  return false;
+};
 
 const start = (input, left, right, result) => {
-    if (left === right) {
-        result[left] = input[left];
-        return result;
-    }
+  if (left === right) {
+    result[left] = input[left];
+    return result;
+  } else if (left > right) {
+    return result;
+  }
 
-    if (left > right) {
-        return result;
-    }
+  if (checkPattern(input[left])) {
+    if (checkPattern(input[right])) {
+      result[left] = input[right];
+      result[right] = input[left];
 
-    if (checkPattern(input[left]) && checkPattern(input[right])) {
-        result[left] = input[right];
-        result[right] = input[left];
-
-        left++;
-        right--;
+      left++;
+      right--;
     } else {
-        if (!checkPattern(input[left])) {
-            result[left] = input[left];
-            left++;
-        }
-
-        if (!checkPattern(input[right])) {
-            result[right] = input[right];
-            right--;
-        }
+      result[right] = input[right];
+      right--;
     }
+  } else {
+    result[left] = input[left];
+    left++;
+  }
 
-    return start(input, left, right, result);
+  return start(input, left, right, result);
 };
 
 function solution8(input) {
-    let left = 0;
-    let right = input.length - 1;
+  let left = 0;
+  let right = input.length - 1;
 
-    let result = [];
+  let result = [];
 
-    return start(input, left, right, result).join("");
+  return start(input, left, right, result).join("");
 }
 
 module.exports = { solution8 };
